cca30b7dd2b45b9c60970c338d3bd38ef0ae30d4
iamsagarr <64728849+iamsagarr@users.noreply.github.com>
2020-06-29 10:46:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 // JavaScript source code
+//this for creating a connection for our mysql database
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     :'localhost',
-  user     : 'root',
-  password : 'swagger4',
-  database : 'userdetails'
+  user     : 'root',                       // your mysql username here
+  password : 'password',                  //your mysql password here
+  database : 'userdetails'                // database name with which you want to create connection
 });
 connection.connect(function(err){
 if(!err) {
