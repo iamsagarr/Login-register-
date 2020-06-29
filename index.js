d51702e7348799bc6a5888aa3d559a6f851d87a2
iamsagarr <64728849+iamsagarr@users.noreply.github.com>
2020-06-29 11:00:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // JavaScript source code
-//this is main source code for this Api,here i imported all the router paths,routers to handle our get and post requests.
+//this is the main source code for this Api,here i imported all the router paths,routers to handle our get and post requests.
 //need to install node modules express,bodyParser.
 var express=require("express");
 var bodyParser=require('body-parser'); 
